58f8ed7aa4e9db50dd4f6c239e3eaab9aa38f28c
Sidd_ <siddharth726@outlook.com>
2024-02-12 21:25:25 -0800

    [FIX] Link component lalalala

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {Link} from "@/components/ui/link";
+import { Link } from "@/components/ui/link";
 
 export default function Home() {
   return (
--- components/ui/link.jsx
+++ components/ui/link.jsx
@@ -1,7 +1,13 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { Link as NextLink } from "next/link";
 
-export function Link({className, href}) {
-    return (
-        <Link className={cn("border-b border-emerald-500 border-2", className)} href={href}>Submit an article</Link>
-    )
+export function Link({ className, href }) {
+  return (
+    <NextLink
+      className={cn("border-b border-emerald-500 border-2", className)}
+      href={href}
+    >
+      Submit an article
+    </NextLink>
+  );
 }
